5e5c1e9c7654fcdb5b4a3607ef50959f0a592eb1
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-04-19 23:36:56 -0300

    parte basico do user

---
+++ backend/src/main/java/com/develcode/backend/models/User.java
@@ -0,0 +1,51 @@
+package com.develcode.backend.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate birthDate;
+
+    private User() {
+    }
+
+    public User(Long id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setDateOfBirth(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
